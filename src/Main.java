88def8e662a7eb7eb2dd7b76046cbad501ecb0b3
Jarek <jarek.malesza@gmail.com>
2022-10-24 20:37:33 +0200

    dodanie zmiennej "name"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String name="Jarek";
-        System.out.println("Hello "+name);
+        String name=" Jarek";
+        int age= 28;
+        System.out.println("Hello" + name +" mam lat " + age);
     }
 }
